ed0eb5e40ce1953f933c755c6cc7f5c1492665fc
Corn207 <halowars2000@gmail.com>
2021-05-07 04:06:45 +0700

    Update TestUnit

--- ImageEditor.TestUnit/MainWindow.xaml.cs
+++ ImageEditor.TestUnit/MainWindow.xaml.cs
@@ -38,41 +38,43 @@ namespace ImageEditor.TestUnit
             InitializeComponent();
         }
 
-        private Bitmap _ogBmp;
-        public Bitmap OgBmp
+        private Bitmap _bmpOriginal = new Bitmap($"{AppDomain.CurrentDomain.BaseDirectory}..\\..\\Assets\\404NotFound.jpg");
+        public Bitmap BmpOriginal
         {
-            get => _ogBmp;
+            get => _bmpOriginal;
             set
             {
-                _ogBmp = value;
+                _bmpOriginal = value;
                 OnPropertyChanged();
             }
         }
+        private double BmpOriginalZoom = 1;
 
-        private Bitmap _processedBmp;
-        public Bitmap ProcessedBmp
+        private Bitmap _bmpProcessed = new Bitmap($"{AppDomain.CurrentDomain.BaseDirectory}..\\..\\Assets\\404NotFound.jpg");
+        public Bitmap BmpProcessed
         {
-            get => _processedBmp;
+            get => _bmpProcessed;
             set
             {
-                _processedBmp = value;
+                _bmpProcessed = value;
                 OnPropertyChanged();
             }
         }
+        private double BmpProcessedZoom = 1;
 
         private void importButton_Click(object sender, RoutedEventArgs e)
         {
             OpenFileDialog openFileDialog = new OpenFileDialog();
             if (openFileDialog.ShowDialog() == true)
             {
-                OgBmp = new Bitmap(openFileDialog.FileName);
+                BmpOriginal = new Bitmap(openFileDialog.FileName);
             }
         }
 
         private void processButton_Click(object sender, RoutedEventArgs e)
         {
-            MainClass mainClass = new MainClass(OgBmp);
-            ProcessedBmp = mainClass.Process();
+            MainClass mainClass = new MainClass(BmpOriginal);
+            BmpProcessed = mainClass.Process();
         }
 
         private void exportButton_Click(object sender, RoutedEventArgs e)
@@ -80,8 +82,44 @@ namespace ImageEditor.TestUnit
             SaveFileDialog dialog = new SaveFileDialog();
             if (dialog.ShowDialog() == true)
             {
-                ProcessedBmp.Save(dialog.FileName, ImageFormat.Png);
+                BmpProcessed.Save(dialog.FileName, ImageFormat.Png);
             }
+        }
+
+        private void SVOg_PreviewMouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            if (e.Delta > 0)
+            {
+                BmpOriginalZoom += 0.2;
+                ImageOg.Width = BmpOriginal.Width * BmpOriginalZoom;
+                ImageOg.Height = BmpOriginal.Height * BmpOriginalZoom;
+            }
+            else
+            {
+                BmpOriginalZoom -= 0.2;
+                if (BmpOriginalZoom < 0.2) BmpOriginalZoom = 0.2;
+                ImageOg.Width = BmpOriginal.Width * BmpOriginalZoom;
+                ImageOg.Height = BmpOriginal.Height * BmpOriginalZoom;
+            }
+            e.Handled = true;
+        }
+
+        private void SVP_PreviewMouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            if (e.Delta > 0)
+            {
+                BmpProcessedZoom += 0.2;
+                ImageP.Width = BmpProcessed.Width * BmpProcessedZoom;
+                ImageP.Height = BmpProcessed.Height * BmpProcessedZoom;
+            }
+            else
+            {
+                BmpProcessedZoom -= 0.2;
+                if (BmpProcessedZoom < 0.2) BmpProcessedZoom = 0.2;
+                ImageP.Width = BmpProcessed.Width * BmpProcessedZoom;
+                ImageP.Height = BmpProcessed.Height * BmpProcessedZoom;
+            }
+            e.Handled = true;
         }
     }
 }
